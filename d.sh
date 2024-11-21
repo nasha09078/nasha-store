deeb71545b42a1dbfbda060932a4a502b2ec0e54
Mohammed Foud <mfoud444@gmail.com>
2024-11-22 01:00:26 +0300

    i commit

--- d.sh
+++ d.sh
@@ -6,6 +6,9 @@ handle_git() {
 }
 
 original_dir=$(pwd)
+DEST_DIR="/home/mfoud444/Desktop/htdocs/htdocs/wordpress/wp-content/themes/nasha-store"
+PHP_THEMES_FOLDER="/home/mfoud444/1project/customar/freelancer/amjad/nasha-store/themes"
+cp -r "${DEST_DIR}" "${PHP_THEMES_FOLDER}/"
 
 cp .envproduction .env
 handle_git
