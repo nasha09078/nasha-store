be66800596225f938acc99c9290c7a9193de55e1
Mohammed Foud <mfoud444@gmail.com>
2024-11-22 01:10:49 +0300

    i commit

--- themes/nasha-store/index.php
+++ themes/nasha-store/index.php
@@ -2,8 +2,8 @@
     $nuxt_index_path = get_template_directory() . '/public/index.html';
     if (file_exists($nuxt_index_path)) {
         $nuxt_content = file_get_contents($nuxt_index_path);
-        // echo $nuxt_content;
-        echo apply_filters('the_content', $nuxt_content);
+        echo $nuxt_content;
+        // echo apply_filters('the_content', $nuxt_content);
     } else {
         echo '<p>Error: Nuxt app index.html file not found.</p>';
     }
